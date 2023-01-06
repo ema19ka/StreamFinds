631ab1da6c3c6e51fb494ea7de189ebd65ca1da6
Eva <evamaria.altenburger@gmail.com>
2023-01-06 10:40:28 +0100

    redo repo w/ api builder

--- app/src/main/java/com/example/streamfinds/data/StreamFindsRepository.kt
+++ app/src/main/java/com/example/streamfinds/data/StreamFindsRepository.kt
@@ -1,9 +1,24 @@
 package com.example.streamfinds.data
+import com.example.streamfinds.network.MovieDbAPI
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 /**
- * Repository that fetch mars photos list from marsApi.
+ * Repository that fetch movies list from movieApi.
  */
-interface StreamFindsRepository {
+//object bc of singleton
 
+object StreamFindsRepository {
+
+    private val api: MovieDbAPI
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        api = retrofit.create(MovieDbAPI::class.java)
+    }
 }

4706e5d5b1d8c91c64b17fcb6d89ff20ba3f7c8c
Eva <evamaria.altenburger@gmail.com>
2023-01-06 10:38:18 +0100

    redo api

--- app/src/main/java/com/example/streamfinds/network/MovieDbAPI.kt
+++ app/src/main/java/com/example/streamfinds/network/MovieDbAPI.kt
@@ -1,31 +1,20 @@
 package com.example.streamfinds.network
 
-import com.example.streamfinds.model.Movie
-import com.example.streamfinds.model.SearchDTO
+import com.example.streamfinds.model.GetMoviesResponse
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val BASE_URL = "https://api.themoviedb.org/3/"
 
 interface MovieDbAPI {
     @GET("search/movie")
-    suspend fun findItems(@Query("api_key") api_key: String, @Query("query") query: String): List<SearchDTO>
+    suspend fun searchMovie(
+        @Query("api_key") api_key: String = "4e3418e89befff40b8dfab831c11e2d9",
+        @Query("query") query: String
+    ): Call<GetMoviesResponse>
 
-    companion object {
-        var apiService: MovieDbAPI? = null
-        fun getInstance(): MovieDbAPI {
-            if (apiService == null) {
-                apiService = Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build().create(MovieDbAPI::class.java)
-            }
-            return apiService!!
-        }
-    }
 }
 
 

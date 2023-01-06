f6ee114cf7ba60c3f5611903ebf0b7d1dffac5b1
Eva <evamaria.altenburger@gmail.com>
2023-01-06 16:07:38 +0100

    prep result screen for data

--- app/src/main/java/com/example/streamfinds/ui/screens/result/ResultScreen.kt
+++ app/src/main/java/com/example/streamfinds/ui/screens/result/ResultScreen.kt
@@ -1,9 +1,13 @@
 package com.example.streamfinds.ui.screens.result
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -13,11 +17,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.streamfinds.R
 import com.example.streamfinds.data.StreamFindsRepository
 import com.example.streamfinds.model.Movie
 import com.example.streamfinds.ui.screens.StreamsViewModel
@@ -87,6 +98,13 @@ fun ResultScreen(
                 Text(text = "Details")
             }
         }*/
+        Box(modifier = modifier.fillMaxSize().padding(10.dp, 25.dp)) {
+            Column{
+                Image(modifier = Modifier.size(200.dp), painter = painterResource(R.drawable.loading_img), contentDescription = "loading")
+                Text(text = "Loading")
+            }
+
+        }
 
     }
 
@@ -98,6 +116,45 @@ private fun onMoviesFetched(movies: List<Movie>) {
 
 private fun onError() {
     Log.d("MainAc", "error")
+}
+
+
+/**
+ * The home screen displaying photo grid.
+ */
+@Composable
+fun MoviesGridScreen(movies: List<Movie>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(items = movies, key = { movie -> movie.id }) { movie ->
+            MoviePosterCard(movie)
+        }
+    }
+}
+
+@Composable
+fun MoviePosterCard(photo: Movie, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .aspectRatio(1f),
+
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(photo.posterPath)
+                .crossfade(true)
+                .build(),
+            error = painterResource(R.drawable.loading_img),
+            placeholder = painterResource(R.drawable.loading_img),
+            contentDescription = stringResource(R.string.test),
+            contentScale = ContentScale.FillBounds,
+        )
+    }
 }
 
 

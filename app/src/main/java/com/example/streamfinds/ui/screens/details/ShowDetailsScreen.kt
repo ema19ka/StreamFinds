92b5e05856101f5152004c1badc2aaa2889f217f
Eva <evamaria.altenburger@gmail.com>
2023-01-15 11:42:37 +0100

    center show title

--- app/src/main/java/com/example/streamfinds/ui/screens/details/ShowDetailsScreen.kt
+++ app/src/main/java/com/example/streamfinds/ui/screens/details/ShowDetailsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
@@ -71,7 +72,18 @@ fun ShowDetails(showDetails: ShowDetails) {
             text = showDetails.title,
             fontWeight = FontWeight.Bold,
             fontSize = 32.sp,
-            modifier = Modifier.padding(12.dp)
+            modifier = Modifier.padding(12.dp),
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    lineHeight = 1.5.em,
+                    lineHeightStyle = LineHeightStyle(
+                        alignment = LineHeightStyle.Alignment.Center,
+                        trim = LineHeightStyle.Trim.None
+                    )
+                )
+            )
+
         )
         Row {
             Text(
